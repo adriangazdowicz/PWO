1f09333001dea2b9ae036eb2ecb1e0a00cc4d182
Adrian <adrian.gazdowicz@gmail.com>
2024-10-10 14:25:49 +0200

    apsockaspo

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Halo1");
         System.out.println("nowy commit");
 
-        System.out.println("Ty będziesz usunięty");
+
+        System.out.println("Nowy feature");
     }
 }
