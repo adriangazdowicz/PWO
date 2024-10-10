23ec49ee4b745b56d44dd2f2a9b2c734c21e1ec1
Adrian <adrian.gazdowicz@gmail.com>
2024-10-10 14:01:01 +0200

    nowa galaz

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HAALLOOO123");
 
-        public Main(); {
-            System.out.println("Constructor124 3");
-        };
+
 
     }
 }
