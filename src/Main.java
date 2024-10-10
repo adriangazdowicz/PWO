38401fae6289e7a5c48ecd193bb27b1503daf475
Adrian <adrian.gazdowicz@gmail.com>
2024-10-10 13:56:03 +0200

    fix/repair sth dnsfndsfidsifnso

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HAALLOOO123");
 
+        public Main(){
+            System.out.println("Constructor");
+        };
 
     }
 }
