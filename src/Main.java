085146ba749de5ffa48931708c8061505cd640f4
Adrian <adrian.gazdowicz@gmail.com>
2024-10-10 13:58:33 +0200

    feat/ task ainasiofna

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HAALLOOO123");
 
-        public Main(){
-            System.out.println("Constructor");
+        public Main(); {
+            System.out.println("Constructor124f 32 3");
         };
 
     }
